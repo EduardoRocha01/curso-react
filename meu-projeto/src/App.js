27615284a470487ea2ded9920e6d28072af4b822
EduardoRocha01 <120874588+EduardoRocha01@users.noreply.github.com>
2023-01-14 19:21:14 -0400

    Update App.js

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 import HelloWord from './components/HelloWords';
 
 function App() {
-  const name = 'mateus'
-
+  
   return (
     <div className="App">
-       <h1>Ola js!</h1>
-       <p>estou aprendendo JS, que legal {name}</p>
+      
+      
        <HelloWord/>
       
     </div>
